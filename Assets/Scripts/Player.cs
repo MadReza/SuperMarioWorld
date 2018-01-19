9c577ad4cf2bf5a7c479a347eb5dd4404b4bc138
MadReza <rezmaza@gmail.com>
2018-01-18 23:51:59 -0500

    Added Player, Animation, Platforms

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float MovementSpeed = 10;
+    public float JumpForce = 10;
+
+    Rigidbody2D rigidbody2d;
+
+	void Start () {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(horizontal, 0, 0) * MovementSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody2d.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+	}
+}
