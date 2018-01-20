6a08df1c6e364be036be123e1dba58d91e205bbf
MadReza <rezmaza@gmail.com>
2018-01-19 22:30:40 -0500

    Added Platform Animation, RootMotion and Draging Player with Transform Parent

---
+++ Assets/Scripts/FloatingBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingBar : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D otherObjectCollider)
+    {
+        if (otherObjectCollider.gameObject.tag == "Player")
+        {
+            otherObjectCollider.transform.parent = gameObject.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D otherObjectCollider)
+    {
+        if (otherObjectCollider.gameObject.tag == "Player")
+        {
+            otherObjectCollider.transform.parent = null;
+        }
+    }
+
+}
