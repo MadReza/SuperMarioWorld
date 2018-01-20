3d20041d05e6335c2c5a7c642cd2df0da6e3ae48
MadReza <rezmaza@gmail.com>
2018-01-19 23:26:25 -0500

    Added Mario Animation/Flip and Animator to control animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,19 +8,73 @@ public class Player : MonoBehaviour {
     public float JumpForce = 10;
 
     Rigidbody2D rigidbody2d;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    //Animator Variables
+    bool walking = false;
 
 	void Start () {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	void Update () {
+        Movement();
+        UpdateAnimator();
+	}
+
+    void UpdateAnimator()
+    {
+        animator.SetBool("Walking", walking);
+        animator.SetBool("Jumping", IsJumping());
+        animator.SetBool("Falling", IsFalling());
+    }
+
+    void Movement()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
 
-        transform.position += new Vector3(horizontal, 0, 0) * MovementSpeed * Time.deltaTime;
+        if (horizontal == 0)
+            walking = false;
+        else
+            walking = true;
 
-        if (Input.GetButtonDown("Jump"))
+        transform.position += new Vector3(horizontal, 0, 0) * MovementSpeed * Time.deltaTime;
+        Flip(horizontal);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rigidbody2d.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
         }
-	}
+    }
+
+    bool IsJumping()
+    {
+        return rigidbody2d.velocity.y > 0;
+    }
+
+    bool IsFalling()
+    {
+        return rigidbody2d.velocity.y < 0;
+    }
+
+    bool IsGrounded()
+    {
+        return rigidbody2d.velocity.y == 0;
+    }
+
+    void Flip(float direction)
+    {
+        if (direction == -1)    //left
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (direction == 1)    //right
+        {
+            spriteRenderer.flipX = false;
+        }
+
+    }
 }
